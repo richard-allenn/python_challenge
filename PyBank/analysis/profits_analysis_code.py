d34b9f9251fe4be46910a1b5c598189429265b43
richard <richard-allen24@outlook.com>
2023-01-03 14:08:59 +0800

    a bit more presentable

--- PyBank/analysis/profits_analysis_code.py
+++ PyBank/analysis/profits_analysis_code.py
@@ -26,34 +26,34 @@ with open(csvpath) as csvfile:
     print(f'there is {month_count} months total')
 
     #total profit / losses
-    total_profits = 1                                                        
+    total_profits = 5934                                                        
     for row in csv_reader:
         total_profits = (row[1]) + total_profits                        #row[1] syntax??
 
-    print(f'the total profit/loss is {total_profits} dollars')
+    print(f'the total profit/loss is ${total_profits}')
 
 
     # the average of total changes
     average_change = 0
     average_change = total_profits / month_count
     
-    print(f'the average change is {average_change}')
+    print(f'the average change is ${average_change}')
 
     # greatest increase in profits
     max_profit = 1234
     for row in csv_reader:
         if float(row[1]) > float(max_profit):                                       # again
             max_profit = row[1]                                                     
-            print(f' the highest profit month is {max_profit}')
+            print(f'the greatest increase in profit is ${max_profit}')
 
 
     # the greatest decrease in profits
     low_profit = 4321
     for row in csv_reader:
         if row[1] < low_profit:
-            print(row)
+            low_profit = row[1]
 
-    print(f'the lowest profit month is {low_profit}')                       #should be one inside if but is here to show rest of code works
+    print(f'the greatest decrease in profit is ${low_profit}')                       #should be one inside  if but is here to show rest of code works
         
 
 
